54171d94f8a7851419443bbfceb41fe3a69ebecf
winning godspower <winninggodspower@gmail.com>
2023-05-26 14:44:54 +0100

    made settings to be chosed from .env file

--- ATHC/wsgi.py
+++ ATHC/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for ATHC project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ATHC.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"ATHC.settings.{os.getenv('ENVIRONMENT')}")
 
 application = get_wsgi_application()

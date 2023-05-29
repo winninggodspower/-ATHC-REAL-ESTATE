dbda6d2fb19405c0b370ef436065df10eea3d517
winning godspower <winninggodspower@gmail.com>
2023-05-29 14:40:30 +0100

    changed media root to an environment variable

--- ATHC/settings/base.py
+++ ATHC/settings/base.py
@@ -145,7 +145,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.getenv("MEDIA_ROOT") or os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
